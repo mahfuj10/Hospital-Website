76a22ff76f77bb694dc2405e2d22c3a0d3e8cfad
mahfuj10 <mahfujglobal@gmail.com>
2021-10-20 23:36:00 +0600

    add slider

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
  website link : https://medwin-cares.web.app
 
  <!-- content -->
- This is a hospital website . It just made on healthcare based. 
+ This is a hospital website . It just made on healthcare based.
+ There have department a also show the doctor detials . Depart details is a private. You can't see the department detials without sign up or login
  
  <!-- facility -->
  I show the all department of hospital. I also show the all doctor of my website .
  You can see the which is doctor is special for your problem. You can hire this person.
  My hospital have 10 department if you click the see more option you can see the detials of this department.
- I use firebase Authentication if you can't sign up you can't see the details of departmetn.
+ I use firebase Authentication of google email sign in and sign up
+ I also show the cost of any treatment
 
   <!-- what i use  -->
   1.React js
@@ -20,3 +22,5 @@
   <!-- website structure -->
   ----> Header --> services ---> department --> appoinment ---> feedback ----> footer
   Router : Home ---> About ---> Doctor ---> Blog ----> Contact 
+
+  ------ Thank you ------
--- src/App.js
+++ src/App.js
@@ -26,23 +26,6 @@ import NoFound from './pages/Nofound/Nofound';
 
 function App() {
 
-     const [sliderRef] = useKeenSlider({
-          spacing: 10,
-          slidesPerView: 1,
-          centered: true,
-          loop: true,
-          mode: "snap",
-          breakpoints: {
-            "(min-width: 768px)": {
-              slidesPerView: 2,
-              mode: "free-snap",
-            },
-            "(min-width: 1200px)": {
-              slidesPerView: 3,
-              mode: "free-snap",
-            },
-          },
-        });
 
       
   return (
@@ -60,86 +43,11 @@ function App() {
        <Route exact path="/">
          
            <Home />
-           <article style={{marginTop:"200px",marginBottom:"200px"}} >
-          <h3 className="text-center mb-5">WHAT OUR PATIENTS SAYS</h3>
-          <div ref={sliderRef} className="keen-slider">
-            <div className=" keen-slider__slide number-slide1">
-            <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src={img1} alt="" />
-            <h5 className="text-center">Rahim Mehedi</h5>
-            <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-                   
-            </div>
-             <div className="keen-slider__slide number-slide2">
-             <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-             </div>
-           <div className="keen-slider__slide number-slide3">
-           <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-           </div>
-           <div className="keen-slider__slide number-slide4">
-           <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-           </div>
-          <div className="keen-slider__slide number-slide5">
-          <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-          </div>
-           <div className="keen-slider__slide number-slide6">
-           <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-           </div>
-          </div>
-          </article>
-        
-          
-       
+     
        </Route>
 
        <Route path="/home">
             <Home />
-
-            <article style={{marginTop:"200px",marginBottom:"200px"}} >
-          <h3 className="text-center mb-5">WHAT OUR PATIENTS SAYS</h3>
-          <div ref={sliderRef} className="keen-slider">
-            <div className=" keen-slider__slide number-slide1">
-            <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg" alt="" />
-            <h5 className="text-center">Rahim Mehedi</h5>
-            <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-                   
-            </div>
-             <div className="keen-slider__slide number-slide2">
-             <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-             </div>
-           <div className="keen-slider__slide number-slide3">
-           <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-           </div>
-           <div className="keen-slider__slide number-slide4">
-           <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-           </div>
-          <div className="keen-slider__slide number-slide5">
-          <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-          </div>
-           <div className="keen-slider__slide number-slide6">
-           <img style={{width:"100px",height:"100px",borderRadius:"50%",marginBottom:'10px',marginLeft:"43%",marginTop:"25px"}} src="https://image.freepik.com/free-photo/mand-holding-cup_1258-340.jpg" alt="" />
-                    <h5 className="text-center">Fahim Salehi</h5>
-                    <p className="w-75 text-center ps-5 ms-5">hank you so much for your kind words, Jane. We really appreciate you taking the time out to share your experience with us</p>
-           </div>
-          </div>
-          </article>
           
        </Route>
 
